cc7580762018b763fca2a4ec5bf27918131328b4
spurs0929 <briankang0929@gmail.com>
2022-07-25 16:19:55 +0800

    add 共用工具函數

---
+++ src/components/Calendar/utils.js
@@ -0,0 +1,26 @@
+/**
+ * 創建tr
+ * @param {數目} count 
+ * @returns 
+ */
+export function createTrs(count){
+  const trArr = [];
+
+  for(let i = 0; i < count; i++){
+    const oTr = document.createElement('tr');
+    trArr.push(oTr);
+  }
+
+  return trArr;
+}
+
+export function getDateInfo(timeStamp){
+  var date = timeStamp ? new Date(timeStamp) : new Date();
+
+  return [
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate()
+  ]
+}
+
